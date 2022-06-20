abf1055884bb7eb2e689d3719975e0b533714124
leobraboo <leonardo.l.souza@ufv.br>
2022-06-20 00:15:03 -0300

    aplication reveal

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,14 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+window.sr = ScrollReveal ({reset: true})
+
+ScrollReveal({
+	reseat : true,
+	distance: '60px',
+	duration: 2000,
+	delay:500
+});
+
+ScrollReveal().reveal('.container' ,{delay:500 , origin:'left'} )
