ac8d4b7bca61c1f173e776f970e17cd561c59ee4
leobraboo <leonardo.l.souza@ufv.br>
2022-06-24 20:58:57 -0300

    reveal

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,17 @@ window.addEventListener('DOMContentLoaded', event => {
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
 
-// window.sr = ScrollReveal ({reset: true})
+window.sr = ScrollReveal ({reset: true})
 
-// ScrollReveal({
-// 	reseat : true,
-// 	distance: '600px',
-// 	duration: 2000,
-// 	delay:400
-// });
+ScrollReveal({
+	reseat : true,
+	distance: '600px',
+	duration: 2000,
+	delay:400
+});
 
-// ScrollReveal().reveal('.site-heading-upper' ,{delay:200 , origin:'left'} )
-// ScrollReveal().reveal('.site-heading-lower' ,{delay:200 , origin:'right'} )
+ScrollReveal().reveal('.site-heading-upper' ,{delay:200 , origin:'left'} )
+ScrollReveal().reveal('.site-heading-lower' ,{delay:200 , origin:'right'} )
 // ScrollReveal().reveal('.navbar' ,{delay:200 , origin:'left'} )
 // ScrollReveal().reveal('.teste' ,{delay:200 , origin:'right'} )
 // ScrollReveal().reveal('.product-item' ,{delay:200 , origin:'right'} )
